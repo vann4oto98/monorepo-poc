b48c47e5d5cc02cd82e6e8d408b99f527aa0943f
Vanyo Ivanov <vanyoivanov98@gmail.com>
2022-03-02 13:08:03 +0200

    rollup helpers

---
+++ packages/helpers/dist/index.cjs.js
@@ -0,0 +1,83 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {};
+
+  var target = _objectWithoutPropertiesLoose(source, excluded);
+
+  var key, i;
+
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source);
+
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i];
+      if (excluded.indexOf(key) >= 0) continue;
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue;
+      target[key] = source[key];
+    }
+  }
+
+  return target;
+}
+
+var _excluded = ["forwardedRef"];
+var withRef = function withRef(Component) {
+  var Wrapped = function Wrapped(props) {
+    var forwardedRef = props.forwardedRef,
+        rest = _objectWithoutProperties(props, _excluded);
+
+    return /*#__PURE__*/React__default["default"].createElement(Component, _extends({
+      ref: forwardedRef
+    }, rest));
+  };
+
+  return /*#__PURE__*/React__default["default"].forwardRef(function (props, ref) {
+    return /*#__PURE__*/React__default["default"].createElement(Wrapped, _extends({}, props, {
+      forwardedRef: ref
+    }));
+  });
+};
+
+exports.withRef = withRef;
